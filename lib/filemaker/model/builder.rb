2d2eaa9a77812b3e649b3b063636d96f8b64559c
John Lannon <john.lannon@livingsocial.com>
2015-03-10 20:57:44 -0400

    Refactor Filemaker::Model::Builder: - extract method hydrated_object_from_record and use   in .collection and .single

--- lib/filemaker/model/builder.rb
+++ lib/filemaker/model/builder.rb
@@ -9,23 +9,7 @@ module Filemaker
         models = []
 
         resultset.each do |record|
-          object = klass.new
-
-          object.instance_variable_set('@new_record', false)
-          object.instance_variable_set('@record_id', record.record_id)
-          object.instance_variable_set('@mod_id', record.mod_id)
-          object.instance_variable_set('@portals', record.portals)
-
-          record.keys.each do |fm_field_name|
-            # record.keys are all lowercase
-            field = klass.find_field_by_name(fm_field_name)
-            next unless field
-
-            # Because we are using ActiveModel::Dirty, so we hydrate directly.
-            object.attributes[field.name] = field.coerce(record[fm_field_name])
-          end
-
-          models << object
+          models << hydrated_object_from_record(klass.new, record, true)
         end
 
         models
@@ -35,6 +19,10 @@ module Filemaker
         record = resultset.first
         object = klass.new
 
+        hydrated_object_from_record(klass.new, resultset.first)
+      end
+
+      def hydrated_object_from_record(object, record, hydrate_directly = false)
         object.instance_variable_set('@new_record', false)
         object.instance_variable_set('@record_id', record.record_id)
         object.instance_variable_set('@mod_id', record.mod_id)
@@ -42,10 +30,15 @@ module Filemaker
 
         record.keys.each do |fm_field_name|
           # record.keys are all lowercase
-          field = klass.find_field_by_name(fm_field_name)
+          field = object.class.find_field_by_name(fm_field_name)
           next unless field
 
-          object.public_send("#{field.name}=", record[fm_field_name])
+          if hydrate_directly
+            # Because we are using ActiveModel::Dirty, so we hydrate directly.
+            object.attributes[field.name] = field.coerce(record[fm_field_name])
+          else
+            object.public_send("#{field.name}=", record[fm_field_name])
+          end
         end
 
         object
