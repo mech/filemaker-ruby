b2a35f7aa8f84e032c0c1bb735b16df7b1ad4025
mech <mech@me.com>
2016-05-27 12:55:55 +0800

    Fix #6 - Default to identity when there is no reference_key

--- lib/filemaker/model/relations/belongs_to.rb
+++ lib/filemaker/model/relations/belongs_to.rb
@@ -25,7 +25,17 @@ module Filemaker
             # Double `==` match entire field
             # If the field value contains underscore or space like 'FM_notified'
             # or 'FM notified', a single `=` may not match correctly.
-            @target = target_class.where(reference_key => "=#{key_value}").first
+
+            # If default reference_key is actually not defined at target_class
+            # we will try to find identity
+            reference_key = target_class.find_field_by_name(reference_key) ||
+                            target_class.identity.try(:name)
+
+            if reference_key
+              @target = target_class.where(reference_key => "=#{key_value}").first
+            else
+              @target = nil
+            end
           end
         end
       end
