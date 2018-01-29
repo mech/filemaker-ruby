f1bec67b676d89a65a223a3debe2a13570882e20
mech <mech@me.com>
2018-01-29 16:11:34 +0800

    Batches for where and in

--- lib/filemaker/model/batches.rb
+++ lib/filemaker/model/batches.rb
@@ -3,6 +3,17 @@ module Filemaker
     module Batches
       def in_batches(batch_size: 200, options: {})
         output = []
+        total = self.in(options).count
+        pages = (total / batch_size.to_f).ceil
+        1.upto(pages) do |page|
+          output.concat self.in(options).per(batch_size).page(page).all
+        end
+
+        output
+      end
+
+      def where_batches(batch_size: 200, options: {})
+        output = []
         total = where(options).count
         pages = (total / batch_size.to_f).ceil
         1.upto(pages) do |page|
