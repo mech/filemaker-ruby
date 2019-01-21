4f11639a8a6aff5c4c1a9f3f7306d241319196ee
mech <mech@me.com>
2019-01-21 11:08:29 +0800

    Allow sleeping when batching

--- lib/filemaker/model/batches.rb
+++ lib/filemaker/model/batches.rb
@@ -1,22 +1,24 @@
 module Filemaker
   module Model
     module Batches
-      def in_batches(batch_size: 200, options: {})
+      def in_batches(batch_size: 200, options: {}, sleep: 0)
         output = []
         total = self.in(options).count
         pages = (total / batch_size.to_f).ceil
         1.upto(pages) do |page|
+          sleep(sleep)
           output.concat self.in(options).per(batch_size).page(page)
         end
 
         output
       end
 
-      def where_batches(batch_size: 200, options: {})
+      def where_batches(batch_size: 200, options: {}, sleep: 0)
         output = []
         total = where(options).count
         pages = (total / batch_size.to_f).ceil
         1.upto(pages) do |page|
+          sleep(sleep)
           output.concat where(options).per(batch_size).page(page)
         end
 
