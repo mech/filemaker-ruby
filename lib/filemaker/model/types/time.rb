75bb9eaf467546b2404bcb79267f2434090f2c88
mech <mech@me.com>
2019-04-24 09:21:29 +0800

    Return value regardless

--- lib/filemaker/model/types/time.rb
+++ lib/filemaker/model/types/time.rb
@@ -12,6 +12,9 @@ module Filemaker
         def self.__filemaker_serialize_for_update(value)
           return nil if value.nil?
           return value.strftime("%H:%M") if value.is_a?(::Time) || value.is_a?(::DateTime)
+
+          # Could be a string like "09:00" already
+          value
         end
 
         def self.__filemaker_serialize_for_query(value)
