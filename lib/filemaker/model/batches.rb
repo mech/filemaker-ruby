d9b8326316423ee016d9365a28c7cd09e8337ae3
mech <mech@me.com>
2021-03-17 12:22:27 +0800

    Allow batching to use omit also

--- lib/filemaker/model/batches.rb
+++ lib/filemaker/model/batches.rb
@@ -1,25 +1,59 @@
 module Filemaker
   module Model
     module Batches
-      def in_batches(batch_size: 200, options: {}, sleep: 0)
+      def in_batches(batch_size: 200, options: {}, include: {}, omit: nil, sleep: 0)
         output = []
-        total = self.in(options).count
+
+        if options.present?
+          include = options
+          warn "Using `options` is deprecated. Please use `include` instead"
+        end
+
+        total = if omit
+          self.in(include).not_in(omit).count
+        else
+          self.in(include).count
+        end
+
         pages = (total / batch_size.to_f).ceil
+
         1.upto(pages) do |page|
           sleep(sleep)
-          output.concat self.in(options).per(batch_size).page(page)
+
+          if omit
+            output.concat(self.in(include).not_in(omit).per(batch_size).page(page))
+          else
+            output.concat(self.in(include).per(batch_size).page(page))
+          end
         end
 
         output
       end
 
-      def where_batches(batch_size: 200, options: {}, sleep: 0)
+      def where_batches(batch_size: 200, options: {}, include: {}, omit: nil, sleep: 0)
         output = []
-        total = where(options).count
+
+        if options.present?
+          include = options
+          warn "Using `options` is deprecated. Please use `include` instead"
+        end
+
+        total = if omit
+          where(include).not(omit).count
+        else
+          where(include).count
+        end
+
         pages = (total / batch_size.to_f).ceil
+
         1.upto(pages) do |page|
           sleep(sleep)
-          output.concat where(options).per(batch_size).page(page)
+
+          if omit
+            output.concat(where(include).not(omit).per(batch_size).page(page))
+          else
+            output.concat(where(include).per(batch_size).page(page))
+          end
         end
 
         output
